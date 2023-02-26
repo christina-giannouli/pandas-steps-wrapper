1aef862da88de8f175432c26c7f010a27f8fc064
christina-giannouli <xristitina@gmail.com>
2023-02-26 14:35:41 +0200

    modify tailwind and ts configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      teal: '#bbffcc',
+      dark: '#262626',
+      black: '#1a1a1a',
+      purple: '#ce90ff',
+      red: '#ff4b55',
+      yellow: '#ffef5d'
+    },
   },
   plugins: [],
 };
